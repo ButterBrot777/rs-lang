b106f531f2747eb0bf6c0a3a6df43fea164d5fbe
NastiaKoval <anasteziyam@gmail.com>
2020-06-24 16:11:48 +0300

    feat: add buttons on dictionary page

--- src/dictionary/dictionaryHeader.js
+++ src/dictionary/dictionaryHeader.js
@@ -4,8 +4,8 @@ class DictionaryHeader extends React.Component {
 
     render() {
         return (
-            <div className="dictionary-header">
-                <button className={this.props.words === "all" ? "btn active" : "btn"} onClick={this.props.getAll}>Изучаемые слова</button>
+            <div className="dictionary-buttons">
+                <button className={this.props.words === "learning" ? "btn active" : "btn"} onClick={this.props.getLearning}>Изучаемые слова</button>
                 <button className={this.props.words === "hard" ? "btn active" : "btn"} onClick={this.props.getHard}>Сложные слова</button>
                 <button className={this.props.words === "deleted" ? "btn active" : "btn"} onClick={this.props.getDeleted}>Удалённые слова</button>
             </div>
--- src/dictionary/dictionaryPage.js
+++ src/dictionary/dictionaryPage.js
@@ -1,67 +1,70 @@
 import React from 'react';
 import Word from './word';
+import DictionaryHeader from './dictionaryHeader'
 
 class Dictionary extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
+            allData: [],
+            currentData: [],
             isLoading: false,
         }
+        this.words = '';
     }
 
-    getUserWords = async () => {
+    componentDidMount = async () => {
+        this.setState({ isLoading: true });
         const user_words_url = `https://afternoon-falls-25894.herokuapp.com/users/${this.props.userId}/words`;
         const rawResponse = await fetch(user_words_url, {
             method: 'GET',
             withCredentials: true,
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZjBhMmUwOTg5NmUxMDAxN2VlYTRlNSIsImlhdCI6MTU5Mjg0NzM0MCwiZXhwIjoxNTkyODYxNzQwfQ.0Hv_TI5axNY3dbZpB72c7_N6xAkpTCuiJ6XoAckaBiQ',
+                'Authorization': `Bearer ${this.props.token}`,
                 'Accept': 'application/json',
             }
         });
         const content = await rawResponse.json();
-        return content;
+        this.setState({allData: content, isLoading: false,})
+        this.getLearning();
     }
 
-    getAll = async () => {
-        const content = await this.getUserWords();
-        this.setState({data: content, isLoading: false,});
+    getLearning = () => {
+        const learningWords = this.state.allData.filter(word => word.optional.deleted===false);
+        console.log(learningWords);
+        this.words = 'learning';
+        this.setState({currentData: learningWords, isLoading: false,});
     }
 
-    getHard = async () => {
-        const content = await this.getUserWords();
-        const hardWords = await content.filter(word => word.difficulty==="hard");
-        this.setState({data: hardWords, isLoading: false,});
+    getHard = () => {
+        const hardWords = this.state.allData.filter(word => word.difficulty==="hard" && word.optional.deleted===false);
+        console.log(hardWords);
+        this.words = 'hard';
+        this.setState({currentData: hardWords, isLoading: false,});
     }
 
-    getDeleted = async () => {
-        const content = await this.getUserWords();
-        const deletedWords = await content.filter(word => word.optional.deleted===true);
+    getDeleted = () => {
+        const deletedWords = this.state.allData.filter(word => word.optional.deleted===true);
         console.log(deletedWords);
-        this.setState({data: deletedWords, isLoading: false,});
-    }
-
-    componentDidMount = async () => {
-        await this.getAll();
+        this.words = 'deleted';
+        this.setState({currentData: deletedWords, isLoading: false,});  
     }
 
     render() {
-        const { data, isLoading } = this.state;
+        const { currentData, isLoading } = this.state;
         if (isLoading) {
             return <p>Loading ...</p>;
         }
         return  (
             <div>
-                <div className="buttons">
-                    <button onClick={this.getAll}>Все слова</button>
-                    <button onClick={this.getHard}>Сложные</button>
-                    <button onClick={this.getDeleted}>Удаленные</button>
-                </div>
+                <header>
+                    <DictionaryHeader words={this.words} getLearning={this.getLearning} 
+                    getHard={this.getHard} getDeleted={this.getDeleted} />
+                </header>
                 
-                <div className="all-words">
-                    {data.map(element => <Word difficulty={element.difficulty} optional={element.optional} wordId={element.wordId} />)}
-                </div>    
+                <div className="words-list">
+                    {currentData.map(element => <Word difficulty={element.difficulty} optional={element.optional} wordId={element.wordId} key={element.wordId} />)}
+                </div>
             </div>
             
         )       
