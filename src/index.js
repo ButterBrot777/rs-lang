42ec07b12ea1b5a1380db1b916055cc3752b6666
Pavel Karenda <korendos@gmail.com>
2020-06-25 20:03:44 +0300

    feat: add basic landing page version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+import Landing from './landing_page/landing_page';
 // import './ComponentSingInAndSingUp.css';
 
 // import AppBtn from './ComponentSingInAndSingUp/AppBtn'
@@ -9,3 +11,10 @@ import App from './AppStructureAndRouter/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Landing />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
