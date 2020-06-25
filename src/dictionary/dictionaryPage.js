1348a539e747df6081106d10025a493aea257d61
NastiaKoval <anasteziyam@gmail.com>
2020-06-26 00:13:12 +0300

    feat: fix all from PR comments

--- src/dictionary/dictionaryPage.js
+++ src/dictionary/dictionaryPage.js
@@ -7,19 +7,18 @@ import './dictionary.css'
 class Dictionary extends React.Component {
     constructor(props) {
         super(props);
-        this.updateAllData = this.updateAllData.bind(this);
         this.state = {
             allData: [],
             currentData: [],
+            words: '',
             isLoading: false,
         }
-        this.words = '';
     }
 
     componentDidMount = async () => {
         this.setState({ isLoading: true });
-        const user_words_url = `https://afternoon-falls-25894.herokuapp.com/users/${this.props.userId}/words`;
-        const rawResponse = await fetch(user_words_url, {
+        const userWordsUrl = `https://afternoon-falls-25894.herokuapp.com/users/${this.props.userId}/words`;
+        const rawResponse = await fetch(userWordsUrl, {
             method: 'GET',
             withCredentials: true,
             headers: {
@@ -28,55 +27,48 @@ class Dictionary extends React.Component {
             }
         });
         const content = await rawResponse.json();
-        console.log(content)
         this.setState({allData: content, isLoading: false,})
         this.getLearning();
     }
 
     getLearning = () => {
         const learningWords = this.state.allData.filter(word => word.optional.deleted===false && word.optional.hardWord===false);
-        console.log(learningWords);
-        this.words = 'learning';
-        this.setState({currentData: learningWords, isLoading: false,});
+        this.setState({currentData: learningWords, words: 'learning', isLoading: false,});
     }
 
     getHard = () => {
         const hardWords = this.state.allData.filter(word => word.optional.hardWord===true);
-        console.log(hardWords);
-        this.words = 'hard';
-        this.setState({currentData: hardWords, isLoading: false,});
+        this.setState({currentData: hardWords, words: 'hard', isLoading: false,});
     }
 
     getDeleted = () => {
         const deletedWords = this.state.allData.filter(word => word.optional.deleted===true);
-        console.log(deletedWords);
-        this.words = 'deleted';
-        this.setState({currentData: deletedWords, isLoading: false,});  
+        this.setState({currentData: deletedWords, words: 'deleted', isLoading: false,});  
     }
 
-    updateAllData = (word_obj) => {
-        const newAllData = this.state.allData.map(word => word.wordId === word_obj.wordId ? word_obj : word);
+    updateAllData = (wordObj) => {
+        const newAllData = this.state.allData.map(word => word.wordId === wordObj.wordId ? wordObj : word);
         this.setState({allData: newAllData, isLoading: false,})
-        this.words === "hard" ? this.getHard() : this.getDeleted();
+        this.state.words === "hard" ? this.getHard() : this.getDeleted();
     }
 
     render() {
-        const { currentData, audio, isLoading } = this.state;
+        const { currentData, words, isLoading } = this.state;
         if (isLoading) {
             return <p>Loading ...</p>;
         }
         return  (
             <div>
                 <header>
-                    <DictionaryHeader words={this.words} getLearning={this.getLearning} 
+                    <DictionaryHeader words={words} getLearning={this.getLearning} 
                     getHard={this.getHard} getDeleted={this.getDeleted} />
                 </header>
                 
                 <div className="words-list">
-                    {currentData.map(element => <Word userId={this.props.userId} token={this.props.token} difficulty={element.difficulty} optional={element.optional} wordId={element.wordId} words={this.words} onWordTypeChange={this.updateAllData} key={element.wordId} />)}
+                    {currentData.map(element => <Word userId={this.props.userId} token={this.props.token} difficulty={element.difficulty} optional={element.optional} wordId={element.wordId} words={words} onWordTypeChange={this.updateAllData} key={element.wordId} />)}
                 </div>
                 <div>
-                    {this.words === "hard" ? <button className="btn train-hard-btn">Повторить</button> : ''}
+                    {words === "hard" ? <button className="dictionary-btn train-hard-btn">Повторить</button> : ''}
                 </div>
                
             </div>
