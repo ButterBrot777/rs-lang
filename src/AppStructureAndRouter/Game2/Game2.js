3bef85ac29c11ef3594c1b82dbeebc72dc626ab1
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-06-29 13:03:30 +0300

    refactor: organize files

--- src/AppStructureAndRouter/Game2/Game2.js
+++ src/AppStructureAndRouter/Game2/Game2.js
@@ -1,20 +1,16 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router,Link} from "react-router-dom";
-import Fade from 'react-reveal/Fade';
+import React, {Component} from "react";
 
-import './Game2.css'
 class Game2 extends Component{
   constructor(){
     super()
+    this.state={
+    
+    }
   }
   render(){
     return(
-      <Fade left opposite>
-      <div className="Game2">
-      <h1>Game2</h1>
-      <Link to="/HomePage"><button>Close Game2</button></Link>
+      <div>
       </div>
-      </Fade> 
     )
   }
 }
