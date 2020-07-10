725d8f716f9df91a308dff748ba3963f0ecca05a
Yuliya Kogol <kogol.julia@gmail.com>
2020-07-10 22:19:42 +0300

    feat: add a check if the game was played today

--- src/AppStructureAndRouter/HomePage/HomePage.js
+++ src/AppStructureAndRouter/HomePage/HomePage.js
@@ -3,23 +3,134 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 import FlavorForm from './InputSelect';
 import 'materialize-css';
 import { Dropdown, Button } from 'react-materialize';
+import { getSettingsUser, addSettingsUser } from '../ServerRequest/ServerRequests';
 
 import './HomePage.scss';
+import '../BasicGame/BasicGame.css'
+
 import StartSettings from './InputSelect';
 
 class HomePage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			maxWordsPerDay: 0,
+			wordsPerDay: 0,
+			redirect: false,
+			isModalWindow: false
+		};
+	}
+
+	componentDidMount = async () => {
+		let { wordsPerDay, optional: { lastTrain, maxWordsPerDay,
+			level, page, wordsLearntPerPage, hints } } = await getSettingsUser();
+		this.setState({
+			maxWordsPerDay,
+			wordsPerDay,
+			level,
+			page,
+			wordsLearntPerPage,
+			lastTrain,
+			hints: {
+				meaningHint: hints.meaningHint,
+				translationHint: hints.translationHint,
+				imageHint: hints.imageHint,
+				transcriptionHint: hints.transcriptionHint,
+				exampleHint: hints.exampleHint,
+				soundHint: hints.soundHint,
+			}
+		})
+	}
+
+	componentDidUpdate = (prevState) => {
+		if (this.state.wordsPerDay !== prevState.wordsPerDay
+			|| this.state.maxWordsPerDay !== prevState.maxWordsPerDay) {
+			this.handleSettingsUpdate();
+		}
+	}
+
+	inputCheck = (event) => {
+		if (/^\d+$/.test(event.target.value) || event.target.value === '') {
+			const target = event.target;
+			const value = target.name === 'maxWordsPerDay' ? target.value : target.value;
+			const name = target.name;
+			this.setState({
+				[name]: +value
+			})
+		}
+	}
+
+	handleSettingsUpdate = () => {
+		let { lastTrain, wordsPerDay, page, level,
+			wordsLearntPerPage, maxWordsPerDay, hints } = this.state;
+		if (wordsPerDay > 0 && maxWordsPerDay > 0) {
+			let newSettings = {
+				"wordsPerDay": wordsPerDay,
+				"optional": {
+					"maxWordsPerDay": maxWordsPerDay,
+					"level": level,
+					"page": page,
+					"wordsLearntPerPage": wordsLearntPerPage,
+					"lastTrain": lastTrain,
+					"hints": {
+						"meaningHint": hints.meaningHint,
+						"translationHint": hints.translationHint,
+						"exampleHint": hints.exampleHint,
+						"soundHint": hints.soundHint,
+						"imageHint": hints.imageHint,
+						"transcriptionHint": hints.transcriptionHint
+					},
+				}
+			};
+			addSettingsUser(newSettings);
+		}
+	}
+
+	handleStartGame = () => {
+		let date = new Date();
+		let today = date.toLocaleDateString();
+		if (this.state.lastTrain === today) {
+			this.setState({
+				isModalWindow: true
+			});
+		} else {
+			this.setState({
+				redirect: true,
+				lastTrain: today
+			});
+		}
+	}
+
+	closeModal = () => {
+		this.setState({
+			isModalWindow: false
+		})
+	}
+
 	render() {
 		return (
 			<div className="wrapper__home-page">
+				{this.state.isModalWindow && (
+					<div className="game-end">
+						<div onClick={this.closeModal}>X</div>
+						<h1 className="info-big">Ура, на сегодня все!</h1>
+						<div className="info-small">Есть еще новые карточки, но дневной лимит исчерпан.</div>
+						<Link className="btn" to="/BasicGame">Play More</Link>
+					</div>
+				)}
 				<div className="wrapper__inner">
 					<div className="home-page">
 						<div className="start-menu">
 							<h2 className="start-menu__logo">Choose Settings:</h2>
-								<StartSettings basicGameWords={this.props.basicGameWords}
-										handleWordsChoice={this.props.handleWordsChoice}/>
+							<StartSettings
+								basicGameWords={this.props.basicGameWords} handleWordsChoice={this.props.handleWordsChoice}
+								redirect={this.state.redirect} handleStartGame={this.handleStartGame}
+								inputCheck={this.inputCheck} handleSettingsUpdate={this.handleSettingsUpdate}
+								maxWordsPerDay={this.state.maxWordsPerDay} wordsPerDay={this.state.wordsPerDay}
+							/>
 						</div>
 						<div className="start-menu">
-              <h2 className="user-info__title">User Info:</h2>
+							<h2 className="user-info__title">User Info:</h2>
 
 							<Link to="/Stat">
 								<span className="button button-settings_bordered">Statistic</span>
--- src/AppStructureAndRouter/HomePage/InputSelect.js
+++ src/AppStructureAndRouter/HomePage/InputSelect.js
@@ -1,81 +1,11 @@
 import React, { Component } from 'react';
-import { getSettingsUser, addSettingsUser } from '../ServerRequest/ServerRequests';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom';
+import { Route, Redirect, Link } from 'react-router-dom';
 export default class StartSettings extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			maxWordsPerDay: 0,
-			wordsPerDay: 0,
-			level: 0,
-			
-		};
-	}
-
-	componentDidMount = async () => {
-		let { wordsPerDay, optional: { maxWordsPerDay, level, page, wordsLearntPerPage, hints } } = await getSettingsUser();
-		this.setState({
-			maxWordsPerDay,
-			wordsPerDay,
-			level,
-			page,
-			wordsLearntPerPage,
-			hints: {
-				meaningHint: hints.meaningHint,
-				translationHint: hints.translationHint,
-				imageHint: hints.imageHint,
-				transcriptionHint: hints.transcriptionHint,
-				exampleHint: hints.exampleHint,
-				soundHint: hints.soundHint,
-			}
-		})
-	}
-
-	componentDidUpdate = (prevState) => {
-		if (this.state.wordsPerDay !== prevState.wordsPerDay
-			|| this.state.maxWordsPerDay !== prevState.maxWordsPerDay) {
-			this.handleSettingsUpdate();
-		}
-	}
-
+	
 	handleSubmit = event => {
 		event.preventDefault();
 	};
-
-	inputCheck = (event) => {
-		if (/^\d+$/.test(event.target.value) || event.target.value === '') {
-			const target = event.target;
-			const value = target.name === 'maxWordsPerDay' ? target.value : target.value;
-			const name = target.name;
-			this.setState({
-				[name]: value
-			})
-		}
-	}
-
-	handleSettingsUpdate = () => {
-		let { wordsPerDay, page, level, wordsLearntPerPage, maxWordsPerDay, hints } = this.state;
-		if (wordsPerDay > 0 && maxWordsPerDay > 0) {
-			let newSettings = {
-				"wordsPerDay": wordsPerDay,
-				"optional": {
-					"maxWordsPerDay": maxWordsPerDay,
-					"level": level,
-					"page": page,
-					"wordsLearntPerPage": wordsLearntPerPage,
-					"hints": {
-						"meaningHint": hints.meaningHint,
-						"translationHint": hints.translationHint,
-						"exampleHint": hints.exampleHint,
-						"soundHint": hints.soundHint,
-						"imageHint": hints.imageHint,
-						"transcriptionHint": hints.transcriptionHint
-					},
-				}
-			};
-			addSettingsUser(newSettings);
-		}
-	}
 
 	render() {
 		return (
@@ -88,8 +18,8 @@ export default class StartSettings extends Component {
 						placeholder="words"
 						autoFocus={true}
 						autoComplete="off"
-						value={this.state.maxWordsPerDay}
-						onChange={(event) => this.inputCheck(event)}
+						value={this.props.maxWordsPerDay}
+						onChange={(event) => this.props.inputCheck(event)}
 						className="word-input_setting"
 					></input>
 				</label>
@@ -99,12 +29,11 @@ export default class StartSettings extends Component {
 						type="text"
 						name="wordsPerDay"
 						autoComplete="off"
-						value={this.state.wordsPerDay}
-						onChange={(event) => this.inputCheck(event)}
+						value={this.props.wordsPerDay}
+						onChange={(event) => this.props.inputCheck(event)}
 						className="word-input_setting"
 					></input>
 				</label>
-
 				<label>
 					words type
 					<select value={this.props.basicGameWords}
@@ -114,13 +43,18 @@ export default class StartSettings extends Component {
 						<option value="combined">combined</option>
 					</select>
 				</label>
-				<Link to="/BasicGame">
+				{/* <Link to="/BasicGame"> */}
+					{
+					this.props.redirect ?
+						<Redirect from="/" to="/BasicGame" /> :
 					<input
 						type="button"
 						value="Start Training"
 						className="button button_bordered button-training "
+						onClick={this.props.handleStartGame}
 					/>
-				</Link>
+				// </ Link >
+					}
 			</form>
 		);
 	}
