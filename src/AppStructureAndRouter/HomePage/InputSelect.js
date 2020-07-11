ade696dd080008104f98ccde2b3ffa970931ae9d
Pavel Karenda <korendos@gmail.com>
2020-07-11 16:58:51 +0300

    feat: implement adaptive design

--- src/AppStructureAndRouter/HomePage/InputSelect.js
+++ src/AppStructureAndRouter/HomePage/InputSelect.js
@@ -26,49 +26,51 @@ export default class StartSettings extends Component {
 		event.preventDefault();
 	};
 
-  inputCheck = (event) => {
-    if(/^\d+$/.test(event.target.value)) {
-      if(event.target.name === 'maxWordsPerDay') this.setState({ totalWordsPerDay: +event.target.value })
-      if(event.target.name === 'newWordsPerDay') this.setState({ newWordsPerDay: +event.target.value })
-    } 
-  }
+	inputCheck = event => {
+		if (/^\d+$/.test(event.target.value)) {
+			if (event.target.name === 'maxWordsPerDay') this.setState({ totalWordsPerDay: +event.target.value });
+			if (event.target.name === 'newWordsPerDay') this.setState({ newWordsPerDay: +event.target.value });
+		}
+	};
 
 	render() {
 		return (
 			<form onSubmit={this.handleSubmit} className="settings-container settings-inputs">
-				<label>
-					max words
-					<input
-						type="text"
-						name="maxWordsPerDay"
-						placeholder="words"
-						autoFocus={true}
-						autoComplete="off"
-						value={this.state.totalWordsPerDay}
-						onChange={(event) => this.inputCheck(event)}
-						className="word-input_setting"
-					></input>
-				</label>
-				<label>
-					new words
-					<input
-						type="text"
-						name="newWordsPerDay"
-						autoComplete="off"
-						value={this.state.newWordsPerDay}
-						onChange={(event) => this.inputCheck(event)}
-						className="word-input_setting"
-					></input>
-				</label>
+				<div className="form-button-container">
+					<label>
+						max words
+						<input
+							type="text"
+							name="maxWordsPerDay"
+							placeholder="words"
+							autoFocus={true}
+							autoComplete="off"
+							value={this.state.totalWordsPerDay}
+							onChange={event => this.inputCheck(event)}
+							className="word-input_setting"
+						></input>
+					</label>
+					<label>
+						new words
+						<input
+							type="text"
+							name="newWordsPerDay"
+							autoComplete="off"
+							value={this.state.newWordsPerDay}
+							onChange={event => this.inputCheck(event)}
+							className="word-input_setting"
+						></input>
+					</label>
 
-				<label>
-					words type
-					<select value={this.state.value} onChange={this.handleChange}>
-						<option value="new">new</option>
-						<option value="learned">learned</option>
-						<option value="combined">combined</option>
-					</select>
-				</label>
+					<label>
+						words type
+						<select value={this.state.value} onChange={this.handleChange}>
+							<option value="new">new</option>
+							<option value="learned">learned</option>
+							<option value="combined">combined</option>
+						</select>
+					</label>
+				</div>
 				<input
 					type="submit"
 					value="Start Training"
